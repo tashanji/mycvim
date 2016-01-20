61038882baaed1568bd180e08d998ecd2a9e2686
tashanji <jitashan@gmail.com>
2016-01-20 11:23:08 +0800

    modify template

--- c-support/codesnippets/main.cc
+++ c-support/codesnippets/main.cc
@@ -5,14 +5,12 @@
 
 using namespace std;
 
-// ===  FUNCTION  ======================================================================
-//         Name:  main
-//  Description:  main function
-// =====================================================================================
-	int
-main ( int argc, char *argv[] )
+/**
+ * @brief               main function
+ **/
+int main ( int argc, char *argv[] )
 {
-	cout	<< "\nProgram " << argv[0] << endl << endl;
+	cout<< "\nProgram " << argv[0] << endl << endl;
 
 	return EXIT_SUCCESS;
 }		// ----------  end of function main  ---------- 
